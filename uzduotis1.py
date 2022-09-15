20bdb7ee63a69107d1b8f31eecf2705e46ae1e48
Donatas Noreika <donoras@gmail.com>
2022-09-15 08:28:42 +0300

    Pridėtas savo logeris

--- uzduotis1.py
+++ uzduotis1.py
@@ -1,38 +1,50 @@
 from math import sqrt
 import logging
 
-logging.basicConfig(filename='uzduotis1.log', encoding='UTF-8',
-                    level=logging.INFO, format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+# logging.basicConfig(filename='uzduotis1.log', encoding='UTF-8',
+#                     level=logging.INFO, format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('uzduotis1.log', encoding="UTF-8")
+logger.addHandler(file_handler)
+
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
 
 def suma(*skaiciai):
     res = sum(skaiciai)
-    logging.info(f'Funkcija {suma.__name__} įvykdyta, skaičių {skaiciai} suma lygi {res}')
+    logger.info(f'Funkcija {suma.__name__} įvykdyta, skaičių {skaiciai} suma lygi {res}')
     return res
 
 
 def saknis(skaicius):
     try:
         res = sqrt(skaicius)
-        logging.info(f'Funkcija {saknis.__name__} įvykdyta, skaičiaus {skaicius} šaknis lygi {res}')
+        logger.info(f'Funkcija {saknis.__name__} įvykdyta, skaičiaus {skaicius} šaknis lygi {res}')
         return res
     except TypeError:
-        logging.exception(f'Funkcija {saknis.__name__} neįvykdyta, paduotas ne skaičius')
+        logger.exception(f'Funkcija {saknis.__name__} neįvykdyta, paduotas ne skaičius')
 
 
 def simboliu_kiekis(sakinys):
     res = len(sakinys)
-    logging.info(f'Funkcija {simboliu_kiekis.__name__} įvykdyta, sakinio {sakinys} ilgis lygus {res}')
+    logger.info(f'Funkcija {simboliu_kiekis.__name__} įvykdyta, sakinio {sakinys} ilgis lygus {res}')
     return res
 
 
 def dalyba(x, y):
     try:
         res = x / y
-        logging.info(f'Funkcija {dalyba.__name__} įvykdyta, {x} / {y} = {res}')
+        logger.info(f'Funkcija {dalyba.__name__} įvykdyta, {x} / {y} = {res}')
         return res
     except ZeroDivisionError:
-        logging.exception(f'Funkcija {dalyba.__name__} neįvykdyta, dalyba iš nulio negalima')
+        logger.exception(f'Funkcija {dalyba.__name__} neįvykdyta, dalyba iš nulio negalima')
 
 
 suma(8, 6, 4, 6)
